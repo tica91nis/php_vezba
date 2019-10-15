6d99e0ebbd3b862a8188a3feab2c2f0a447169f5
tica91nis <tijanajovic2012@gmail.com>
2019-10-15 16:57:53 +0200

    zadnji cas

--- if_naredba.php
+++ if_naredba.php
@@ -1,5 +1,17 @@
 <html>
 <head>
+    <style>
+    img  {
+        width: 200px;
+        height: 200px;
+    }
+    .z {
+        color: pink;
+    }
+    .m {
+        color: blue;
+    }
+    </style>
 </head>
 <body>
     <?php
@@ -15,6 +27,35 @@
             echo "a je manje od b <br>";
             echo "$a je manje od $b";
         }
+        echo "<br>";
+        $x = 3;
+        $y = 5;
+        if($x > $y)
+        {
+            echo "$x je vece od $y";
+        }
+        elseif($x < $y)
+        {
+            echo "$x je manje od $y";
+        }
+        else 
+        {
+            echo "$x je jednako$y";
+        }
+
+    //u odnosu na pol da prikaze odredjenu slicicu
+    echo "<br>";
+    $pol = "z";
+    if($pol == "z")
+    {
+        echo "<img src='woman.png'>";
+        echo "<p class='z'> Zenski pol </p>";
+    }
+    else
+    {
+        echo "<img src='man.png'>";
+        echo "<p class='m'> Muski pol </p>";
+    }
     ?>
 </body>
 </html>
